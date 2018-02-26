c39bfc07b9580da92cca0062d3d84bc28befc83c
DrHypercube <wdcook4@gmail.com>
2018-02-26 18:39:59 -0500

    Forgot some more parentheses.

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Paddle:
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300 )
         self.x = 0
-        self.canvas_width = self.canvas.winfo_width
+        self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
     def draw(self):
