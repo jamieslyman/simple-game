9d3a6ba43b5d83375809c7ae14fe383ff69fb6de
DrHypercube <wdcook4@gmail.com>
2018-02-26 18:32:18 -0500

    forgot ANOTHER COLON

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Ball:
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
         return False
     def draw(self):
