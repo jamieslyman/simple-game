94b781c6bf50a6bdf59f7747baa52cc0a47f55d1
DrHypercube <wdcook4@gmail.com>
2018-02-26 18:35:36 -0500

    Man I suck. I forgot colons, used - instead of =, and suck at copying. And importing apparently.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter as Tk
 from tkinter import *
 import random
 import time
@@ -17,7 +18,7 @@ class Ball:
         self.hit_bottom = False
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
-        if pos[2] >= paddle_pos[0] and pos[0] <= paddle[2]:
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
         return False
@@ -30,7 +31,7 @@ class Ball:
             self.hit_bottom - True
         if self.hit_paddle(pos) == True:
             self.y = -3
-        if.pos[0] <= 0:
+        if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
@@ -54,7 +55,7 @@ class Paddle:
         self.x = -2
     def turn_right(self, evt):
         self.x = 2
-tk - Tk()
+tk = Tk()
 tk.title("Game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
